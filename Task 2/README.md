5d2ee250285bcbf23efe9cbe2351a8d35286ab2e
solomiapobutska <60586815+solomiapobutska@users.noreply.github.com>
2020-09-27 22:03:45 -0400

    Add files via upload

---
+++ Task 2/README.md
@@ -0,0 +1,3 @@
+## Task 2
+
+### For this task, I created a shell script from the data gathered in Task 1.
---
+++ Task 2/shellscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Shell Script for Task 1
+
+#Get total number of songs:  $ wc -l spotifydata.csv
+#Sort in reverse: sort -r spotifydaa.csv
+#Extracting top ten songs and placing them to the new file: head -n 12 spotifydata.csv > topten.txt
+#Extract all artist from the list: cat mycsv.csv | cut -d ',' -f3
